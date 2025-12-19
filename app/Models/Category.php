6f7d05ae979d15ce7637398d93a29492311ab6d9
Ybec <yermybenjaminec@gmail.com>
2025-12-18 19:52:21 -0500

    Category y Lotecontroller culminados

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,12 +10,15 @@ class Category extends Model
 {
     use HasFactory;
     // La tabla no tiene created_at/updated_at
+    protected $primaryKey = 'Id';
+    public $incrementing = true; // o false si no es autoincrement
+    protected $keyType = 'int'; 
     public $timestamps = false;
     protected $table = 'categoria'; // Cambia si tu tabla se llama diferente
 
     protected $fillable = [
-        'nombre',
-        'descripcion'
+        'Nombre',
+        'Descripcion'
     ];
 
     public function productos()
